506a6539d0f292bcb65905a114290010d1e41410
gianhacker <mrgianhacker@gmail.com>
2022-07-11 22:18:00 +0530

    add all files without missing anything

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
-from fastapi import FastAPI
+from lib2to3.pytree import Base
+from fastapi import Body, FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
+
 
 app = FastAPI()
 
+class Posts(BaseModel):
+    title: str
+    content: str
+
+class Student(BaseModel):
+    
+
 @app.get("/")
-async def root():
+async def get_user():
     return{"message:":"Hello World !"}
+
+@app.get("/posts")
+async def get_posts():
+    return{"data:":"Welcome to the API world !"}
+
+@app.post("/createPosts")
+async def createPosts(payLoad: dict = Body(...)):
+    print(payLoad)
+    #return{"PayLoad":"successfully created the post"}
+    return{"new_post:": f"title: {payLoad['title']} content: {payLoad['content']}"}
